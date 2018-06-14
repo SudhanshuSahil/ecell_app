eef3e026c64376d6af95c229da6f9919c254eb9a
rishabhdeadpool <rishabhramtekegsr@gmail.com>
2018-06-14 22:07:16 +0530

    user model work started

--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Events(models.Model):
+
+
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    ldap_id = models.CharField(max_length=50, null=True, blank=True)
+    roll_no = models.CharField(max_length=10, null=True, blank=True)
+    profile_pic = models.URLField(null=True, blank=True)
+    fcm_id = models.CharField(max_length=200, null=True, blank=True)
+    Interested_Events=models.ForeignKey(Events, on_delete=models.CASCADE)
+    Going_Events = models.ForeignKey(Events, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, blank=True)
+    contact_number=models.BigAutoField(null=True, blank=True)
+    
