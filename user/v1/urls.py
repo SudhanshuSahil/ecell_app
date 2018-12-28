16e0e69c8b991483a825df97273e1b77573516c7
itachi uchiha <itachi@test.ecell.in>
2018-12-28 22:21:26 +0530

    trying

--- user/v1/urls.py
+++ user/v1/urls.py
@@ -4,9 +4,9 @@ from user.v1 import views
 
 urlpatterns = [
     # url(r'^user/myevent_add$', views.Myeventsinuser.as_view(), name='v1_myevent_add'),
-    url(r'^api/user/userlist$', views.UserListall.as_view(), name='users-api'),
     url(r'^user/register/', views.UserRegistration.as_view(),
         name='v1_UserRegistration'),
+    url(r'^api/user/userlist/', views.UserListall.as_view(), name='users-api'),
     url(r'^user/login/', views.UserLogin.as_view(), name='v1_UserLogin'),
     url(r'^user/logout/$', views.UserLogout.as_view(), name='v1_UserLogout'),
 
