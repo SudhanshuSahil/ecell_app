f51f8c6ac81e4ea034e39ba6ced4df81fe178056
asdf <asdf>
2019-01-02 23:41:44 +0530

    speakers fields changed

--- speakers/models.py
+++ speakers/models.py
@@ -5,9 +5,10 @@ from common.models import LifeTimeTrackingModel, ActiveModel
 # Create your models here.
 class Speaker(ActiveModel):
     name = models.CharField(max_length=200, blank=True, null=True, default=None)
-    link = models.URLField(blank=True, null=True, default=None)
-    photo = models.URLField(blank=True, null=True, default=None)
-    category = models.CharField(max_length=200,blank=True, null=True, default=None)
+    limkedin_link = models.URLField(blank=True, null=True, default=None)
+    photo_url = models.URLField(blank=True, null=True, default=None)
+    company = models.CharField(max_length=200,blank=True, null=True, default=None)
+    designation = models.CharField(max_length=200,blank=True, null=True, default=None)
 
     class Meta:
         verbose_name = "Speaker"
